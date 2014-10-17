c3fed36d8bd69e07c5a569864bd224e43fde8eda
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-10-17 15:51:38 +0200

    Added lots of config options

--- src/nebula.js
+++ src/nebula.js
@@ -4,160 +4,154 @@
  *
  * @author Alejandro U. Alvarez (http://urbanoalvarez.es)
  */
-var Nebula = function () {
-	// CONFIGURATION
-	this.resolution = 20;
-	this.tolerance = 0; // 0 to 1
-	this.minRad = 0;
-	this.maxRad = 40;
-	this.maxInitRad = 0.0001;
-	this.maxSpeed = 0.8;
-	this.variation = 0.2;
-	this.explosionRadius = 100;
-	this.attraction = 0.00001; // Attraction towards their real center, based on distance
-	this.explosionForce = 0.01; // Explosive force
-	this.mode = 'colorful'; // Normal or colorful
+var Nebula = function (options) {
 
-	// ATTRIBUTES
-	this.canvas = {
-		ctx: $('#bg')[0].getContext("2d"),
-		WIDTH: $("#bg").width(),
-		HEIGHT: $("#bg").height(),
-		canvasMinX: $("#bg").offset().left,
-		canvasMaxX: this.canvasMinX + this.WIDTH,
-		canvasMinY: $("#bg").offset().top,
-		canvasMaxY: this.canvasMinY + this.HEIGHT
-	}
-
-	this.mouse = {
-		s: {
-			x: 0,
-			y: 0
-		}, // Mouse speed
-		p: {
-			x: 0,
-			y: 0
-		} // Mouse position
-	}
-
-	this.content = {
-		text: null,
-		recalculate: true,
-		size: 0
+	var settings = {
+		container: $('canvas'),
+		resolution: 20,
+		tolerance: 0, // 0 to 1
+		minRad: 0,
+		maxRad: 40,
+		maxInitRad: 0.0001,
+		maxSpeed: 0.8,
+		variation: 0.2,
+		explosionRadius: 100,
+		attraction: 0.00001, // Attraction towards their real center, based on distance
+		explosionForce: 0.01, // Explosive force
+		mode: 'colorful', // Normal or colorful
+		debug: true,
+		drawFn: 'rectangle',
+		showForce: false,
+		showDistance: false,
+		wait: 1,
+		showEdges: false,
+		resolutionScale: 35,
+		radLimitScale: 10600,
+		fadeAmount: 0.1,
+		fadeColor: 'rgb(0,0,0)'
 	};
 
-	this.edges = new Array();
-	this.modeChanged = true;
-	this.explode = {
-		x: null,
-		y: null,
-		do :
-		false
-	}
+	var nebula = {},
+		drawingFn = {};
+
+	settings = $.extend(settings, options);
+
+	// Internal attributes
+	var canvas = {
+			ctx: settings.container[0].getContext("2d"),
+			WIDTH: settings.container.width(),
+			HEIGHT: settings.container.height(),
+			canvasMinX: settings.container.offset().left,
+			canvasMaxX: settings.container.offset().left + settings.container.width(),
+			canvasMinY: settings.container.offset().top,
+			canvasMaxY: settings.container.offset().top + settings.container.height()
+		},
+		mouse = {
+			// Mouse speed
+			s: {
+				x: 0,
+				y: 0
+			},
+			// Mouse position
+			p: {
+				x: 0,
+				y: 0
+			}
+		},
+		content = {
+			text: null,
+			recalculate: true,
+			size: 0
+		},
+		edges = new Array(),
+		modeChanged = true,
+		explode = {
+			x: null,
+			y: null,
+			do :
+			false
+		}
+
 	// METHODS
-	this.fade = function () {
-		if (!this.fadeOut) return true;
-		this.canvas.ctx.fillStyle = "rgba(0,0,0," + this.fadeAmount + ")";
-		this.canvas.ctx.fillRect(0, 0, this.canvas.WIDTH, this.canvas.HEIGHT);
-	}
-
-	this.clear = function () {
-		// Clear canvas
-		this.canvas.ctx.clearRect(0, 0, this.canvas.WIDTH, this.canvas.HEIGHT);
-	}
-
-	this.drawText = function (callback) {
-		console.log('drawText');
-		if (this.content.recalculate) {
-			console.log('Recalculating size');
-			this.content.size = 1;
+	nebula.drawText = function (callback) {
+		debug('drawText');
+		if (content.recalculate) {
+			debug('Recalculating size');
+			content.size = 1;
 			var textSize;
 			// Fit text:
 			do {
-				this.canvas.ctx.font = "bold " + this.content.size + "px sans-serif";
-				textSize = this.canvas.ctx.measureText(this.content.text);
-				this.content.size++;
-			} while (textSize.width < this.canvas.WIDTH && this.content.size < this.canvas.HEIGHT);
+				canvas.ctx.font = "bold " + content.size + "px sans-serif";
+				textSize = canvas.ctx.measureText(content.text);
+				content.size++;
+			} while (textSize.width < canvas.WIDTH && content.size < canvas.HEIGHT);
 			// Size done, recalculating resolution
-			this.resolution = Math.ceil(this.content.size / 35);
-			//this.minRad = Math.ceil(28240/this.content.size);
-			this.maxRad = Math.ceil(10590 / this.content.size);
-			console.log('Using resolution: ' + this.resolution);
+			settings.resolution = Math.ceil(content.size / settings.resolutionScale);
+			settings.maxRad = Math.ceil(settings.radLimitScale / content.size);
+			debug('Using resolution: ' + settings.resolution);
 		}
 
-		console.log('Using size: ' + this.content.size);
+		debug('Using size: ' + content.size);
 
-		this.content.recalculate = false;
+		content.recalculate = false;
 
-		this.canvas.ctx.font = "bold " + this.content.size + "px sans-serif";
-		this.canvas.ctx.fillStyle = "#fff";
-		this.canvas.ctx.fillText(this.content.text, 0, this.content.size);
+		canvas.ctx.font = "bold " + content.size + "px sans-serif";
+		canvas.ctx.fillStyle = "#fff";
+		canvas.ctx.fillText(content.text, 0, content.size);
 
 		callback.call();
 	}
 
-	this.write = function (content, effect, extra) {
+	nebula.write = function (text, colors) {
 		// Store text
-		this.content.text = content;
-		console.log('Writing ' + this.content.text);
-		this.resizeCanvas();
-		this.clear();
-		this.drawText($.proxy(function () {
-			switch (effect) {
-			case 'circles':
-				this.findEdges(extra);
-			}
-		}, this));
+		content.text = text;
+		debug('Writing ' + content.text);
+		nebula.resizeCanvas();
+		clear();
+		nebula.drawText(function () {
+			nebula.findEdges(colors);
+		});
 	}
 
-	this.resizeCanvas = function () {
-		this.content.recalculate = true;
+	nebula.resizeCanvas = function () {
+		content.recalculate = true;
 
-		this.canvas.WIDTH = window.innerWidth;
-		this.canvas.HEIGHT = window.innerHeight;
+		canvas.WIDTH = window.innerWidth;
+		canvas.HEIGHT = window.innerHeight;
 
-		$("#bg").attr('width', this.canvas.WIDTH);
-		$("#bg").attr('height', this.canvas.HEIGHT);
+		settings.container.attr('width', canvas.WIDTH);
+		settings.container.attr('height', canvas.HEIGHT);
 	}
 
-	this.newColor = function () {
+	nebula.newColor = function () {
 		return 'rgba(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.random() + ')';
 	}
 
-	this.circle = function (x, y, rad, color) {
-		// Circulo
-		this.canvas.ctx.fillStyle = color;
-		this.canvas.ctx.beginPath();
-		this.canvas.ctx.arc(x, y, rad, 0, Math.PI * 2, true);
-		this.canvas.ctx.closePath();
-		this.canvas.ctx.fill();
+	nebula.mouseMove = function (e) {
+		mouse.s.x = Math.max(Math.min(e.pageX - mouse.p.x, 40), -40);
+		mouse.s.y = Math.max(Math.min(e.pageY - mouse.p.y, 40), -40);
+
+		mouse.p.x = e.pageX - canvas.canvasMinX;
+		mouse.p.y = e.pageY - canvas.canvasMinY;
 	}
 
-	this.mouseMove = function (e) {
-		this.mouse.s.x = Math.max(Math.min(e.pageX - this.mouse.p.x, 40), -40);
-		this.mouse.s.y = Math.max(Math.min(e.pageY - this.mouse.p.y, 40), -40);
-
-		this.mouse.p.x = e.pageX - this.canvas.canvasMinX;
-		this.mouse.p.y = e.pageY - this.canvas.canvasMinY;
+	nebula.logCoords = function (e) {
+		debug([e.pageX - canvas.canvasMinX, e.pageY - canvas.canvasMinY]);
 	}
 
-	this.logCoords = function (e) {
-		console.log([e.pageX - this.canvas.canvasMinX, e.pageY - this.canvas.canvasMinY]);
-	}
-
-	this.findEdges = function (colors) {
-		console.log('Finding edges');
+	nebula.findEdges = function (colors) {
+		debug('Finding edges');
 		// Sweep image finding the coordinates of the edges
-		var pix = this.canvas.ctx.getImageData(0, 0, this.canvas.WIDTH, this.canvas.HEIGHT);
+		var pix = canvas.ctx.getImageData(0, 0, canvas.WIDTH, canvas.HEIGHT);
 		//
-		for (var y = 0; y < pix.height; y += this.resolution) {
-			for (var x = 0; x < pix.width; x += this.resolution) {
+		for (var y = 0; y < pix.height; y += settings.resolution) {
+			for (var x = 0; x < pix.width; x += settings.resolution) {
 				// Generate square average
 				var found = false,
 					auxAvg = 0,
 					points = 0;
-				for (var x1 = 0; x1 < this.resolution; x1++) {
-					for (var y1 = 0; y1 < this.resolution; y1++) {
+				for (var x1 = 0; x1 < settings.resolution; x1++) {
+					for (var y1 = 0; y1 < settings.resolution; y1++) {
 						// I now have all needed pointers
 						// Get the index inside pix array
 						var pixIndex = ((y + y1) * pix.width + x + x1) * 4;
@@ -165,54 +159,54 @@ var Nebula = function () {
 						points++;
 					}
 					auxAvg = auxAvg / points;
-					//if(auxAvg>0) console.log(auxAvg);
-					if (auxAvg > 0 && auxAvg < 255 - (255 * this.tolerance)) {
+					//if(auxAvg>0) debug(auxAvg);
+					if (auxAvg > 0 && auxAvg < 255 - (255 * settings.tolerance)) {
 						found = true;
 						break;
 					}
 				}
 				if (found) {
-					console.log('Edge found');
 					// Found edge, store coordinates
-					this.edges.push([Math.round((x + this.resolution) / 2) * 2, Math.round((y + this.resolution) / 2) * 2 + this.canvas.HEIGHT / 2 - this.content.size / 1.3]);
+					edges.push([Math.round((x + settings.resolution) / 2) * 2, Math.round((y + settings.resolution) / 2) * 2 + canvas.HEIGHT / 2 - content.size / 1.3]);
 				}
 			}
 		}
-		console.log(this.edges);
-		this.circleEdges(colors, new Array());
+		debug(edges);
+		clear();
+		nebula.drawNodes(colors, new Array());
 	}
 
 	// Generate an explosion at the coordinates defined
 	// by event e
-	this.explosion = function (e) {
-		this.explode = {
+	nebula.explosion = function (e) {
+		explode = {
 			x: e.pageX,
 			y: e.pageY,
 			do :
 			true
 		}
-		console.log(this.explode);
+		debug("Explode: ", explode);
 	}
 
 	// Sets a property
-	this.set = function (name, val) {
+	nebula.set = function (name, val) {
 		switch (name) {
 		case 'mode':
-			this.mode = val;
-			this.modeChanged = true;
+			settings.mode = val;
+			settings.modeChanged = true;
 			break;
 		}
 	}
 
-	this.applyColors = function (colors, circles) {
-		if (this.mode == 'colorful') {
+	nebula.applyColors = function (colors, circles) {
+		if (settings.mode == 'colorful') {
 			for (var a = 0; a < colors.length; a++) {
-				var randColor = this.newColor();
+				var randColor = nebula.newColor();
 				for (var i = 0; i < total; i++) {
 					circles[a][i].color = randColor;
 				}
 			}
-		} else if (this.mode == 'normal') {
+		} else if (settings.mode == 'normal') {
 			for (var a = 0; a < colors.length; a++) {
 				for (var i = 0; i < total; i++) {
 					circles[a][i].color = colors[a];
@@ -222,125 +216,195 @@ var Nebula = function () {
 	}
 
 	// Approximate function for distance calc
-	this.distance = function (dx, dy) {
+	nebula.distance = function (dx, dy) {
 		return 1.426776695 * Math.min(0.7071067812 * (Math.abs(dx) + Math.abs(dy)), Math.max(Math.abs(dx), Math.abs(dy)));
 	}
 
 	// Effect: Circles
-	// Draw randomly growing circles on each edge
-	this.circleEdges = function (colors, circles) {
-		// Circles contains one circles array for each color
-		var total = this.edges.length;
-		this.clear();
+	// Draw randomly growing nodes on each edge
+	nebula.drawNodes = function (colors, nodes) {
+		var total = edges.length;
+		if (settings.fadeAmount > 0) {
+			fade();
+		} else {
+			clear();
+		}
+
+
 		for (var a = 0; a < colors.length; a++) {
 			// Check if they have been built yet
-			if (circles[a] == undefined) {
-				console.log('Setting up arrays');
-				circles[a] = new Array;
+			if (nodes[a] == undefined) {
+				debug('Setting up arrays');
+				nodes[a] = new Array;
 				for (var i = 0; i < total; i++) {
 					// dx and dy are speed of the circle
 					// because the circle will revolve around its real center
 					// following 'gravitational' laws
-					circles[a].push({
-						rad: Math.random() * this.maxInitRad,
-						dx: Math.sin(Math.random() * 100 + i + a),
-						dy: Math.cos(Math.random() * 100 + i + a),
-						x: Math.random() * this.canvas.WIDTH,
-						y: Math.random() * this.canvas.HEIGHT,
+					nodes[a].push({
+						rad: Math.random() * settings.maxInitRad,
+						dx: Math.sin(Math.random() * 360 + i + a),
+						dy: Math.cos(Math.random() * 360 + i + a),
+						x: Math.random() * canvas.WIDTH,
+						y: Math.random() * canvas.HEIGHT,
 						color: colors[a]
 					});
 				}
 			}
 
 			var nextColor;
-			if (this.modeChanged) {
+			if (settings.modeChanged) {
 				nextColor = colors[a];
-				if (this.mode == 'colorful') nextColor = this.newColor();
+				if (settings.mode == 'colorful') nextColor = nebula.newColor();
 			} else {
 				// Mutate the color a little bit
-				var current = circles[a][0].color,
+				var current = nodes[a][0].color,
 					c = current.substring(current.indexOf('(') + 1, current.lastIndexOf(')')).split(/,\s*/);
 				// And set the color
 				nextColor = 'rgba(' + Math.round(Math.max(0, Math.min(parseInt(c[0]) + Math.sin(Math.random() * 180), 255))) + ',' + Math.round(Math.max(0, Math.min(parseInt(c[1]) + Math.cos(Math.random() * 180), 255))) + ',' + Math.round(Math.max(0, Math.min(parseInt(c[2]) + Math.sin(Math.random() * 180), 255))) + ',' + Math.max(Math.min(parseFloat(c[3]) + Math.cos(Math.random() * 180 * parseFloat(c[3])) * 0.005, 1), 0) + ')';
-				//debugger;
 			}
 
 			for (var i = 0; i < total; i++) {
 				// Handle color changes
-				circles[a][i].color = nextColor;
-				circles[a][i].rad += Math.sin(Math.random() * 180 + i) * this.variation;
+				nodes[a][i].color = nextColor;
+				nodes[a][i].rad += Math.sin(Math.random() * 180 + i) * settings.variation;
 
-				if (circles[a][i].rad < this.minRad) circles[a][i].rad = this.minRad;
-				if (circles[a][i].rad > this.maxRad) circles[a][i].rad = this.maxRad;
+				if (nodes[a][i].rad < settings.minRad) nodes[a][i].rad = settings.minRad;
+				if (nodes[a][i].rad > settings.maxRad) nodes[a][i].rad = settings.maxRad;
 
 				// Update speed if explosion happened nearby
-				if (this.explode.do) {
-					var dx = circles[a][i].x - this.explode.x,
-						dy = circles[a][i].y - this.explode.y,
-						explosion = this.distance(dx, dy);
-					if (explosion < this.explosionRadius) {
+				if (explode.do) {
+					var dx = nodes[a][i].x - explode.x,
+						dy = nodes[a][i].y - explode.y,
+						explosion = nebula.distance(dx, dy);
+					if (explosion < settings.explosionRadius) {
 						// The force vector is away from the explosion center
 						// and the explosion force is based on the distance to it
 						var force = {
-							x: dx * explosion * this.explosionForce / circles[a][i].rad,
-							y: dy * explosion * this.explosionForce / circles[a][i].rad
+							x: dx * explosion * settings.explosionForce / nodes[a][i].rad,
+							y: dy * explosion * settings.explosionForce / nodes[a][i].rad
 						};
 
 						// Update speed
-						circles[a][i].dx = force.x;
-						circles[a][i].dy = force.y;
+						nodes[a][i].dx = force.x;
+						nodes[a][i].dy = force.y;
 
-						circles[a][i].color = 'rgba(255,255,255,1)';
+						nodes[a][i].color = 'rgba(255,255,255,1)';
 					}
 				}
 
 				// Update position
-				circles[a][i].x += circles[a][i].dx;
-				circles[a][i].y += circles[a][i].dy;
+				nodes[a][i].x += nodes[a][i].dx;
+				nodes[a][i].y += nodes[a][i].dy;
 
 				// Attraction delta, vector from circle center to real center
 				var delta = {
-					x: this.edges[i][0] - circles[a][i].x,
-					y: this.edges[i][1] - circles[a][i].y
+					x: edges[i][0] - nodes[a][i].x,
+					y: edges[i][1] - nodes[a][i].y
 				};
 
 				// Calculate vector force based on distance from centers
-				var gravity = this.distance(delta.x, delta.y);
+				var gravity = nebula.distance(delta.x, delta.y);
 
 				// Attraction force
 				var force = {
-					x: delta.x * gravity * this.attraction * circles[a][i].rad * Math.random(),
-					y: delta.y * gravity * this.attraction * circles[a][i].rad * Math.random()
+					x: delta.x * gravity * settings.attraction * nodes[a][i].rad * Math.random(),
+					y: delta.y * gravity * settings.attraction * nodes[a][i].rad * Math.random()
 				};
 				// Update speed
-				circles[a][i].dx += force.x;
-				circles[a][i].dy += force.y;
+				nodes[a][i].dx += force.x;
+				nodes[a][i].dy += force.y;
 
-				if (circles[a][i].dx > this.maxSpeed) circles[a][i].dx = this.maxSpeed;
-				if (circles[a][i].dy > this.maxSpeed) circles[a][i].dy = this.maxSpeed;
+				if (nodes[a][i].dx > settings.maxSpeed) nodes[a][i].dx = settings.maxSpeed;
+				if (nodes[a][i].dy > settings.maxSpeed) nodes[a][i].dy = settings.maxSpeed;
+
+				// Mark edges
+				if (settings.showEdges) {
+					drawingFn.circle(edges[i][0], edges[i][1], 10, 'rgba(255,255,255,0.01)', canvas);
+				}
 
 
-				/*// Line from center to center
-				this.canvas.ctx.strokeStyle = 'rgba(255,255,255,0.01)';
-				this.canvas.ctx.moveTo(this.edges[i][0],this.edges[i][1]);
-				this.canvas.ctx.lineTo(circles[a][i].x,circles[a][i].y);
-				this.canvas.ctx.stroke();
-				
+				// Line from center to center
+				if (settings.showDistance) {
+					canvas.ctx.strokeStyle = 'rgba(255,255,255,0.01)';
+					canvas.ctx.moveTo(edges[i][0], edges[i][1]);
+					canvas.ctx.lineTo(nodes[a][i].x, nodes[a][i].y);
+					canvas.ctx.stroke();
+				}
+
 				// Forces
-				this.canvas.ctx.strokeStyle = 'rgba(0,255,0,0.01)';
-				this.canvas.ctx.moveTo(circles[a][i].x,circles[a][i].y);
-				this.canvas.ctx.lineTo(circles[a][i].x+circles[a][i].dx*gravity*0.1,circles[a][i].y+circles[a][i].dy*gravity*0.1);
-				this.canvas.ctx.stroke();*/
+				if (settings.showForce) {
+					canvas.ctx.strokeStyle = 'rgba(0,255,0,0.01)';
+					canvas.ctx.moveTo(nodes[a][i].x, nodes[a][i].y);
+					canvas.ctx.lineTo(nodes[a][i].x + nodes[a][i].dx * gravity * 0.1, nodes[a][i].y + nodes[a][i].dy * gravity * 0.1);
+					canvas.ctx.stroke();
+				}
 
 
 				// Draw
-				this.circle(circles[a][i].x, circles[a][i].y, circles[a][i].rad, circles[a][i].color);
+				if (typeof settings.drawFn === 'function') {
+					settings.drawFn(nodes[a][i].x, nodes[a][i].y, nodes[a][i].rad, nodes[a][i].color, canvas);
+				} else {
+					var fn = drawingFn[settings.drawFn];
+					if (typeof fn === 'function') {
+						fn(nodes[a][i].x, nodes[a][i].y, nodes[a][i].rad, nodes[a][i].color, canvas);
+					} else {
+						console.error("The drawing function specified is invalid");
+						return;
+					}
+				}
+
 			}
 		}
-		if (this.modeChanged) this.modeChanged = false;
-		if (this.explode.do) this.explode.do = false;
-		setTimeout($.proxy(function () {
-			this.circleEdges(colors, circles);
-		}, this), 1);
+		if (settings.modeChanged) settings.modeChanged = false;
+		if (explode.do) explode.do = false;
+
+		setTimeout(function () {
+			nebula.drawNodes(colors, nodes);
+		}, settings.wait);
 	}
+
+	/* Drawing functions */
+
+	drawingFn.circle = function (x, y, rad, color, canvasObj) {
+		// Circulo
+		canvasObj.ctx.fillStyle = color;
+		canvasObj.ctx.beginPath();
+		canvasObj.ctx.arc(x, y, rad, 0, Math.PI * 2, true);
+		canvasObj.ctx.closePath();
+		canvasObj.ctx.fill();
+	}
+
+	drawingFn.rectangle = function (x, y, rad, color, canvasObj) {
+		// Circulo
+		canvasObj.ctx.fillStyle = color;
+		canvasObj.ctx.fillRect(x - rad, y - rad, rad * 2, rad * 2);
+	}
+
+	/* Internal functions */
+
+	function fade() {
+		var color = getRGB(settings.fadeColor);
+		canvas.ctx.fillStyle = "rgba(" + color.r + "," + color.g + "," + color.b + "," + settings.fadeAmount + ")";
+		canvas.ctx.fillRect(0, 0, canvas.WIDTH, canvas.HEIGHT);
+	}
+
+	function clear() {
+		// Clear canvas
+		canvas.ctx.clearRect(0, 0, canvas.WIDTH, canvas.HEIGHT);
+	}
+
+	function getRGB(color) {
+		var rgb = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		return {
+			r: rgb[1],
+			g: rgb[2],
+			b: rgb[3]
+		};
+	}
+
+	function debug() {
+		if (settings.debug) console.log.apply(console, arguments);
+	}
+
+	return nebula;
 }
